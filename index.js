fb412fed758c88f5fbef9fa2097edd5e4b93cc62
bunny899 <143364666+bunny899@users.noreply.github.com>
2024-02-21 21:53:24 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const Discord = require('discord.js'); //import client from discord
-
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
@@ -8,15 +7,22 @@ client.on('ready', () => {
 
 client.on('message', msg => {
     // check if message isn't from us
-    if (msg.author == client.user) {
+    if (msg.author === client.user) {
       return;
     }
-    else if (msg.content === 'ping') {
-      msg.reply('Pong!');
-    }
-    else {
-        msg.reply(msg.content);
+    else if (msg.content.startsWith('/say')) {
+      // Extract the message after "/say"
+      const messageToSay = msg.content.slice(5).trim();
+      
+      // Check if there's actually a message to say
+      if (messageToSay) {
+        // Respond with the message prefixed with "message:"
+        msg.channel.send('message: ' + messageToSay);
+      } else {
+        // If no message provided after "/say", inform the user
+        msg.reply('You need to provide a message after "/say"');
+      }
     }
 });
 
-client.login(process.env.TOKEN); //login bot using token
+client.login(process.env.TOKEN);
